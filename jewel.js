1b0709dfbbd7700f143ecf1046ca44f5a97520a9
Krisha316 <106575135+Krisha316@users.noreply.github.com>
2024-12-08 20:20:22 +0530

    Update jewel.js

--- jewel.js
+++ jewel.js
@@ -1,26 +1,26 @@
-// let cart = [];
-// let price = [];
-// let totalprice = 0;
+let cart = [];
+let price = [];
+let totalprice = 0;
 
 
-// function CartDisplay() {
-//     const displayContainer = document.getElementById('cartDisplay');
-//     displayContainer.innerHTML = ''; 
-//     cart.forEach(product => {
-//         const listItem = document.createElement('li');
-//         listItem.innerText = product
-//         displayContainer.appendChild(listItem);
-//     });
-// }
-// function PriceDisplay() {
-//     const displayContainer = document.getElementById('priceDisplay');
-//     displayContainer.innerHTML = ''; 
-//     price.forEach(prices => {
-//         const listItem = document.createElement('li');
-//         listItem.innerText = prices
-//         displayContainer.appendChild(listItem);
-//     });
-// }
+function CartDisplay() {
+    const displayContainer = document.getElementById('cartDisplay');
+    displayContainer.innerHTML = ''; 
+    cart.forEach(product => {
+        const listItem = document.createElement('li');
+        listItem.innerText = product
+        displayContainer.appendChild(listItem);
+    });
+}
+function PriceDisplay() {
+    const displayContainer = document.getElementById('priceDisplay');
+    displayContainer.innerHTML = ''; 
+    price.forEach(prices => {
+        const listItem = document.createElement('li');
+        listItem.innerText = prices
+        displayContainer.appendChild(listItem);
+    });
+}
 
 
 function Item(checkbox, messageClass) {
@@ -29,17 +29,17 @@ function Item(checkbox, messageClass) {
     if (checkbox.checked) {
       message.innerText = `Added to cart!`;
       if (!cart.includes(itemName)) {
-        //cart.push(itemName);
-        //price.push(itemPrice);
+        cart.push(itemName);
+        price.push(itemPrice);
       }
     } else { 
       message.innerText = '';
-      //cart = cart.filter(item => item !== itemName);
-      //price = price.filter((_, index) => price[index] !== itemPrice);
+      cart = cart.filter(item => item !== itemName);
+      price = price.filter((_, index) => price[index] !== itemPrice);
     }
 
-    //CartDisplay();
-    //PriceDisplay();
+    CartDisplay();
+    PriceDisplay();
   }
 
  
